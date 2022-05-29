4ff3372cf5ae3fb9fd508e8ab3173d1a5597d858
rachappa-web-tech <97609401+rachappa-web-tech@users.noreply.github.com>
2022-05-29 10:20:34 +0530

    material-ui added in react-app

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
+    <>
     <div className="App">
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +22,8 @@ function App() {
         </a>
       </header>
     </div>
+    
+    </>
   );
 }
 
