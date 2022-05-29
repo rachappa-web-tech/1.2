8b82e5763bd1403216b4d28811ac31532e3980ed
rachappa-web-tech <97609401+rachappa-web-tech@users.noreply.github.com>
2022-05-29 21:57:56 +0530

    login and signup page form working

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,11 +2,21 @@ import React from 'react';
 import Grid from '@mui/material/Grid'
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import { Avatar, Paper, TextField, FormControlLabel, Checkbox,Link, Button, Typography } from '@mui/material';
+import {Formik,Form,Field} from 'formik';
 
 const Login=()=>{
     const paperstyle={padding:20,height:'70vh',width:280,margin:"20px auto"};
     const avatarstyle={backgroundColor:'green'};
     const textfiledstyle={padding:8};
+    const initialValues ={
+      email:'',
+      password:'',
+      remember:false
+    };
+    const onsubmit=(values,props)=>{
+      console.log(values);
+ 
+    };
     return(
         <>
        <Grid>
@@ -18,46 +28,18 @@ const Login=()=>{
                  </Avatar>
 
                 <h2>Sign in</h2>
-                <TextField style={textfiledstyle}
-                  id=""
-                  type={'uemail'}
-                  label="Email"
-                  placeholder='Enter UserEmail'
-                  variant="outlined"     
-                               required
-                 
-
-                  
-                />
-                   <TextField style={textfiledstyle}
-                  id=""
-                  type={'password'}
-                  label="Password"
-                  placeholder='Password'
-                  required
-                  variant="outlined"
-                  
-
-                  
-                />
-
-
-                <FormControlLabel
-                  label="Remember me"
-                  control={
-                    <Checkbox
-                     name='checkdB'
-                      color="primary"
-                    />
-                  }
-                />
-                <Grid>
-                    <Button variant="text" color="primary" variant="contained" fullWidth type='Submit'>
-                    Sign 
-                  
-                    </Button>
-
-                </Grid>
+                <Formik initialValues={initialValues} onSubmit={onsubmit}>
+                  {(props)=>(
+                    <Form>
+                     
+                      < Field as={TextField} style={textfiledstyle} id="" name='email' type={'email'} label="Email" placeholder='Email' required  variant="outlined"/>
+                      < Field as={TextField} style={textfiledstyle} id="" name='password' type={'password'} label="Password" placeholder='Password' required  variant="outlined"/>
+                      < Field as={FormControlLabel} label="Remember me" name='remember' control={<Checkbox color="primary"/>}/>
+                      <Button  color="primary" variant="contained" fullWidth type='Submit'> Sign </Button>
+                     </Form>
+                      )}
+                 </Formik>
+              
                 <Grid>
                 <Typography style={textfiledstyle} variant="body2" color="initial">Forgot Password ?</Typography>
 
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Grid from '@mui/material/Grid'
 import { Avatar, Paper, Typography, TextField, Button } from '@mui/material';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { Form, Formik,Field } from 'formik';
 
 
 
@@ -9,6 +10,19 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 const Singup=()=>{
     const paperstyle={padding:'30px 20px' ,width:300,height:600,margin:"20px auto" };
     const textfieldstyle ={ padding:'8px'};
+    const initialValues ={
+      Name:'',
+      email:'',
+      phonenumber:'',
+      password:'',
+      repassword:'',
+      adhar:'',
+    };
+    const onsubmit=(values,props)=>{
+      console.log(values);
+ 
+    };
+
     return(
         <>
         <Grid>
@@ -21,51 +35,24 @@ const Singup=()=>{
                     <Typography variant="captionn" color="initial">Please fill this form to create an account</Typography>
                 </Grid>
                 <Grid align='center'>
-                <form>
-
-                    <TextField style={textfieldstyle}
-                      label="Name"  
-                      fullWidth
-                      required
-                    />
-                    <TextField style={textfieldstyle}
-                      label="Email"  
-                      fullWidth
-                      required
-                    />
-                      <TextField style={textfieldstyle}
-                
-                      label="Phone Number"  
-                      fullWidth
-                      required
-                    />
-                  <TextField style={textfieldstyle}
-                    
-                      label="Password"  
-                      fullWidth
-                      required
-                    />
-                     <TextField style={textfieldstyle}
-                   
-                      label="Confirm Password"  
-                      fullWidth
-                      required
-                    />
-                <TextField style={textfieldstyle}
-                      label="Adhara Number"  
-                      fullWidth
-                      required
-                    />
-                    <Button  variant="contained" type='submit' color="primary">Sign Up
-                      
-                    </Button>
-                </form>
-                </Grid>
-
-            </Paper>
-        
-          
-        </Grid>
+ <Formik initialValues={initialValues} onSubmit={onsubmit}>
+   {(props)=>(
+     <Form>
+        <Field as={TextField} style={textfieldstyle} label="Name" name='Name' type='name'fullWidth required/>
+        <Field as={TextField} style={textfieldstyle} label="Email"name='email' type='email'fullWidth required/>
+        <Field as={TextField} style={textfieldstyle} label="Phone Number"name='phonenumber'type='number' fullWidth required/>
+        <Field as={TextField}d style={textfieldstyle} label="Password" name='password' type='password' fullWidth required/>
+        <Field as={TextField} style={textfieldstyle} label="Confirm Password" name='repassword' type='password' fullWidth required/>
+        <Field as={TextField} style={textfieldstyle}  label="Adhara Number" name='adhar' type='number'fullWidth required/>
+        <Button  variant="contained" type='submit' color="primary">Sign Up</Button>
+     </Form>
+   )}
+   
+   
+   </Formik> 
+</Grid>
+  </Paper>
+  </Grid>
 
       
         </>
